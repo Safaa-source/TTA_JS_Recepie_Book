c7fffcf9281fbb29c34c3f099e7738bf3981bfba
Safaa-source <safay410@gmail.com>
2024-11-02 16:21:48 +0200

    feat:seperated the view

--- script.js
+++ script.js
@@ -4,19 +4,23 @@ const displayRecipe = () => {
     const recipeList = document.querySelector("#recipeList");
     recipeList.innerHTML = "";
 
-    recipes.forEach((recipe, index) => {
-        const recipeCard = document.createElement("div");
-        recipeCard.classList.add("bg-white", "p-4", "rounded", "shadow", "mb-4");
+    if (recipeList) {
 
-        recipeCard.innerHTML = `
-        <h2 class="text-lg font-bold">${recipe.title}</h2>
-        <h2 class="text-sm font-semithin text-gray-700"><strong>Ingredients: </strong>${recipe.ingredients}</h2>
-        <h2 class="text-sm font-thin"><strong>Steps: </strong>${recipe.steps}</h2>
-        <button class="bg-blue-500 text-white px-2 py-1 rounded mt-2" onclick="editRecipe(${index})">Edit</button>
-        <button class="bg-red-500 text-white px-2 py-1 rounded mt-2" onclick="deleteRecipe(${index})">Delete</button>
-        `;
-        recipeList.appendChild(recipeCard);
-    })
+        recipes.forEach((recipe, index) => {
+            const recipeCard = document.createElement("div");
+            recipeCard.classList.add("bg-white", "p-4", "rounded", "shadow", "mb-4");
+
+            recipeCard.innerHTML = `
+            <h2 class="text-lg font-bold">${recipe.title}</h2>
+            <h2 class="text-sm font-semithin text-gray-700"><strong>Ingredients: </strong>${recipe.ingredients}</h2>
+            <h2 class="text-sm font-thin"><strong>Steps: </strong>${recipe.steps}</h2>
+            <button class="bg-blue-500 text-white px-2 py-1 rounded mt-2" onclick="editRecipe(${index})">Edit</button>
+            <button class="bg-red-500 text-white px-2 py-1 rounded mt-2" onclick="deleteRecipe(${index})">Delete</button>
+            `;
+            recipeList.appendChild(recipeCard);
+        })
+
+    }
 }
 
 const saveRecipeToLocalStorage = () => {
@@ -26,15 +30,17 @@ const saveRecipeToLocalStorage = () => {
 const loadRecipesFromLocalStorage = () => {
     const storedRecipes = localStorage.getItem("recipes");
 
-    if (storedRecipes){
+    if (storedRecipes) {
         recipes = JSON.parse(storedRecipes)
     }
 }
 
 const showError = (elementId, message) => {
     const errorElement = document.getElementById(elementId);
-    errorElement.innerText = message;
-    errorElement.classList.remove("hidden");
+    if (errorElement) {
+        errorElement.innerText = message;
+        errorElement.classList.remove("hidden");
+    }
 }
 
 const hideError = (elementId) => {
@@ -48,6 +54,8 @@ const addRecipe = (event) => {
     const recipeTitle = document.getElementById("recipeTitle").value.trim();
     const recipeIngredients = document.getElementById("recipeIngredients").value.trim();
     const recipeSteps = document.getElementById("recipeSteps").value.trim();
+
+
 
     // if (recipeTitle !== "" && recipeIngredients !== "" && recipeSteps !== "") {
     hideError("titleError");
@@ -99,7 +107,7 @@ const editRecipe = (index) => {
     const updatedRecipeIngredients = prompt("Enter the new recioe ingredients", recipes[index].ingredients);
     const updatedRecipeSteps = prompt("Enter the new recipe steps", recipes[index].steps);
 
-    if (updatedRecipeTitle && updatedRecipeIngredients && updatedRecipeSteps){
+    if (updatedRecipeTitle && updatedRecipeIngredients && updatedRecipeSteps) {
         recipes[index].title = updatedRecipeTitle;
         recipes[index].ingredients = updatedRecipeIngredients;
         recipes[index].steps = updatedRecipeSteps;
@@ -115,7 +123,11 @@ const deleteRecipe = (index) => {
     displayRecipe();
 }
 
-document.getElementById("recipeForm").addEventListener("submit", addRecipe);
+const recipeForm = document.getElementById("recipeForm");
+
+if (recipeForm) {
+    document.getElementById("recipeForm").addEventListener("submit", addRecipe);
+}
 
 loadRecipesFromLocalStorage();
 displayRecipe();
